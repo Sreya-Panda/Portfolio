ff0a1a49ac018660d60ae134a1298c4f9b7c0e85
Sreya-Panda <147271419+Sreya-Panda@users.noreply.github.com>
2023-10-08 00:03:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggleButton = document.querySelector('.toggle-button');
+const body = document.querySelector('body');
+
+toggleButton.addEventListener('click', () => {
+  body.classList.toggle('dark-mode');
+});
